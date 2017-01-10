8c4136ab1e3a00070f2e8a0c09201ee8e7517540
Mike DuVall <mike.a.duvall@gmail.com>
2017-01-10 07:15:29 -0600

    Update to use same URL for GET and POST gdiMinigunner

--- src/test/groovy/client/MikeAndConquerGameClient.groovy
+++ src/test/groovy/client/MikeAndConquerGameClient.groovy
@@ -18,7 +18,7 @@ class MikeAndConquerGameClient {
 
     GDIMinigunner getGDIMinigunner() {
         RESTClient  restClient = new RESTClient( 'http://localhost:11369' )
-        def resp = restClient.get( path : '/getMinigunner' ) // ACME boomerang
+        def resp = restClient.get( path : '/gdiMinigunner' ) // ACME boomerang
         assert resp.status == 200  // HTTP response code; 404 means not found, etc.
         GDIMinigunner minigunner = new GDIMinigunner()
         minigunner.x = resp.responseData.x
