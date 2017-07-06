6548874ca144e73be389467e5de74672dd052e8a
Mike DuVall <mike.a.duvall@gmail.com>
2017-07-06 07:02:29 -0500

    Update test

--- src/test/groovy/main/MikeAndConquerTest1.groovy
+++ src/test/groovy/main/MikeAndConquerTest1.groovy
@@ -79,6 +79,45 @@ class MikeAndConquerTest1 extends Specification {
 
     }
 
+    def "Nod minigunner should wait 8 seconds and then attack GDI minigunner" () {
+
+        given:
+        gameClient.addGDIMinigunner(300,700)
+        gameClient.addNODMinigunner(1000,300)
+
+
+        when:
+        sleep(3000)
+
+        then:
+        Minigunner gdiMinigunner = gameClient.getGDIMinigunner()
+        assert gdiMinigunner.x == 300
+        assert gdiMinigunner.y == 700
+
+
+        and:
+        Minigunner nodMinigunner = gameClient.getNODMinigunner()
+        assert nodMinigunner.x == 1000
+        assert nodMinigunner.y == 300
+        assert nodMinigunner.health == 1000
+
+        then:
+        def conditions = new PollingConditions(timeout: 30, initialDelay: 1.5, factor: 1.25)
+        conditions.eventually {
+            def expectedDeadMinigunner = gameClient.getGDIMinigunner()
+            assert expectedDeadMinigunner.health == 0
+        }
+
+        when:
+        nodMinigunner = gameClient.getNODMinigunner()
+
+        then:
+        assert nodMinigunner.x < 1000
+        assert nodMinigunner.y > 300
+        assert nodMinigunner.health == 1000
+
+
+    }
 
 
 
