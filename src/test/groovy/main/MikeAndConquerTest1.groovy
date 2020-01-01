5e36cedfdc6079bcfa2d521b17ba7c05a709a32d
mike-duvall <mike.a.duvall@gmail.com>
2020-01-01 11:00:54 -0600

    Refactoring

--- src/test/groovy/main/MikeAndConquerTest1.groovy
+++ src/test/groovy/main/MikeAndConquerTest1.groovy
@@ -88,56 +88,147 @@ class MikeAndConquerTest1 extends Specification {
     }
 
 
+////    @Ignore
+//    def "screenshot of opening game map shroud" () {
+//
+//        given:
+//        int screenshotCompareWidth = 216
+//        int screenshotCompareHeight = 96
+//
+//        // move mouse out of screenshot
+//        gameClient.moveMouseToWorldCoordinates(new Point(screenshotCompareWidth + 50,screenshotCompareHeight + 50))
+//
+//        File imageFile = new File(
+//                getClass().getClassLoader().getResource("real-game-shroud-1-start-x408-y240-216x96.png").getFile()
+//        );
+//        BufferedImage realGameScreenshot = ImageIO.read(imageFile)
+//
+//
+//        when:
+//        BufferedImage fullScreenShot = gameClient.getScreenshot()
+//
+//        then:
+//        BufferedImage screenshotSubImage = fullScreenShot.getSubimage(408,240,screenshotCompareWidth,screenshotCompareHeight)
+//
+//        writeImageToFileInBuildDirectory(screenshotSubImage, "mike-and-conquer-actual-shroud-1-start-x408-y240-216x96.png" )
+//        writeImageToFileInBuildDirectory(realGameScreenshot, "real-game-copied-shroud-1-start-x408-y240-216x96.png" )
+//
+//        and:
+//        assert ImageUtil.imagesAreEqual(screenshotSubImage, realGameScreenshot)
+//
+//        when:
+//        Minigunner minigunner = gameClient.addGDIMinigunnerAtMapSquare(21,12)
+//        gameClient.deleteGdiMinigunnerById(minigunner.id)
+//
+//        and:
+//        imageFile = new File(
+//                getClass().getClassLoader().getResource("real-game-shroud-2-start-x408-y240-216x96.png").getFile()
+//        );
+//        realGameScreenshot = ImageIO.read(imageFile)
+//
+//        and:
+//        fullScreenShot = gameClient.getScreenshot()
+//        screenshotSubImage = fullScreenShot.getSubimage(408,240,screenshotCompareWidth,screenshotCompareHeight)
+//
+//        then:
+//        writeImageToFileInBuildDirectory(screenshotSubImage, "mike-and-conquer-actual-shroud-2-start-x408-y240-216x96.png" )
+//        writeImageToFileInBuildDirectory(realGameScreenshot, "real-game-copied-shroud-2-start-x408-y240-216x96.png" )
+//        assert ImageUtil.imagesAreEqual(screenshotSubImage, realGameScreenshot)
+//
+//
+//
+//    }
+
 //    @Ignore
     def "screenshot of opening game map shroud" () {
 
         given:
-        int screenshotCompareWidth = 216
-        int screenshotCompareHeight = 96
-
-        // move mouse out of screenshot
-        gameClient.moveMouseToWorldCoordinates(new Point(screenshotCompareWidth + 50,screenshotCompareHeight + 50))
-
-        File imageFile = new File(
-                getClass().getClassLoader().getResource("real-game-shroud-1-start-x408-y240-216x96.png").getFile()
-        );
-        BufferedImage realGameScreenshot = ImageIO.read(imageFile)
-
+        int startX
+        int startY
+        int screenshotCompareWidth
+        int screenshotCompareHeight
+        int index
 
         when:
-        BufferedImage fullScreenShot = gameClient.getScreenshot()
+        startX = 408
+        startY = 240
+        screenshotCompareWidth = 216
+        screenshotCompareHeight = 96
+        index = 1
 
         then:
-        BufferedImage screenshotSubImage = fullScreenShot.getSubimage(408,240,screenshotCompareWidth,screenshotCompareHeight)
-
-        writeImageToFileInBuildDirectory(screenshotSubImage, "mike-and-conquer-actual-shroud-1-start-x408-y240-216x96.png" )
-        writeImageToFileInBuildDirectory(realGameScreenshot, "real-game-copied-shroud-1-start-x408-y240-216x96.png" )
-
-        and:
-        assert ImageUtil.imagesAreEqual(screenshotSubImage, realGameScreenshot)
+        assertScreenshotMatches(index, startX, startY, screenshotCompareWidth, screenshotCompareHeight)
 
         when:
+        index = 2
         Minigunner minigunner = gameClient.addGDIMinigunnerAtMapSquare(21,12)
         gameClient.deleteGdiMinigunnerById(minigunner.id)
 
-        and:
-        imageFile = new File(
-                getClass().getClassLoader().getResource("real-game-shroud-2-start-x408-y240-216x96.png").getFile()
-        );
-        realGameScreenshot = ImageIO.read(imageFile)
-
-        and:
-        fullScreenShot = gameClient.getScreenshot()
-        screenshotSubImage = fullScreenShot.getSubimage(408,240,screenshotCompareWidth,screenshotCompareHeight)
-
         then:
-        writeImageToFileInBuildDirectory(screenshotSubImage, "mike-and-conquer-actual-shroud-2-start-x408-y240-216x96.png" )
-        writeImageToFileInBuildDirectory(realGameScreenshot, "real-game-copied-shroud-2-start-x408-y240-216x96.png" )
+        assertScreenshotMatches(index, startX , startY, screenshotCompareWidth, screenshotCompareHeight)
+    }
+
+    void assertScreenshotMatches(int index, int startX, int startY, int screenshotCompareWidth, int screenshotCompareHeight) {
+
+        gameClient.moveMouseToWorldCoordinates(new Point(startX + screenshotCompareWidth + 50,startY + screenshotCompareHeight + 50))
+
+        String realGameFilename = "real-game-shroud-" + index + "-start-x" + startX + "-y" + startY + "-" + screenshotCompareWidth + "x" + screenshotCompareHeight + ".png"
+
+        File imageFile = new File(
+                getClass().getClassLoader().getResource(realGameFilename).getFile()
+        );
+
+        BufferedImage realGameScreenshot = ImageIO.read(imageFile)
+
+        BufferedImage fullScreenShot = gameClient.getScreenshot()
+        BufferedImage screenshotSubImage = fullScreenShot.getSubimage(startX,startY,screenshotCompareWidth,screenshotCompareHeight)
+
+        String realGameCopiedFilename = realGameFilename.replaceAll("real-game-shroud", "real-game-shroud-copied")
+        String mikeAndConquerCopiedFilename = realGameFilename.replaceAll("real-game-shroud", "mike-and-conquer-shroud-actual")
+
+        writeImageToFileInBuildDirectory(realGameScreenshot, realGameCopiedFilename )
+        writeImageToFileInBuildDirectory(screenshotSubImage, mikeAndConquerCopiedFilename )
+
         assert ImageUtil.imagesAreEqual(screenshotSubImage, realGameScreenshot)
 
 
-
     }
+
+//    void assertScreenshotMatches(int index, int startX, int startY, int screenshotCompareWidth, int screenshotCompareHeight) {
+//        gameClient.moveMouseToWorldCoordinates(new Point(startX + screenshotCompareWidth + 50,startY + screenshotCompareHeight + 50))
+//
+//        String realGameFilename = "real-game-shroud-" + index + "-start-x" + startX + "-y" + startY + "-" + screenshotCompareWidth + "x" + screenshotCompareHeight + ".png"
+//
+////        File imageFile = new File(
+////                getClass().getClassLoader().getResource("real-game-shroud-1-start-x408-y240-216x96.png").getFile()
+////        );
+//        File imageFile = new File(
+//                getClass().getClassLoader().getResource(realGameFilename).getFile()
+//        );
+//
+//        BufferedImage realGameScreenshot = ImageIO.read(imageFile)
+//
+//
+//        when:
+//        BufferedImage fullScreenShot = gameClient.getScreenshot()
+//
+//        then:
+//        BufferedImage screenshotSubImage = fullScreenShot.getSubimage(startX,startY,screenshotCompareWidth,screenshotCompareHeight)
+//
+//        String realGameCopiedFilename = realGameFilename.replaceAll("real-game-shroud", "real-game-shroud-copied")
+//        String mikeAndConquerCopiedFilename = realGameFilename.replaceAll("real-game-shroud", "mike-and-conquer-shroud-actual")
+//
+////        writeImageToFileInBuildDirectory(screenshotSubImage, "mike-and-conquer-actual-shroud-1-start-x408-y240-216x96.png" )
+////        writeImageToFileInBuildDirectory(realGameScreenshot, "real-game-copied-shroud-1-start-x408-y240-216x96.png" )
+//        writeImageToFileInBuildDirectory(realGameScreenshot, realGameCopiedFilename )
+//        writeImageToFileInBuildDirectory(screenshotSubImage, mikeAndConquerCopiedFilename )
+//
+//        and:
+//        assert ImageUtil.imagesAreEqual(screenshotSubImage, realGameScreenshot)
+//
+//
+//    }
+
 
 
 
