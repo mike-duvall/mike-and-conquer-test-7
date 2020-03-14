36a44554ec97605f2e12f1ba8d6219c113ffcb53
mike-duvall <mike.a.duvall@gmail.com>
2020-03-14 09:13:49 -0500

    Added another shroud test

--- src/test/groovy/main/ShroudTests.groovy
+++ src/test/groovy/main/ShroudTests.groovy
@@ -427,6 +427,33 @@ class ShroudTests extends Specification {
         assertScreenshotMatches(testScenarioNumber, startX , startY, screenshotCompareWidth, screenshotCompareHeight)
     }
 
+    def "Shroud screenshot scenario 16"() {
+        given:
+        ResetOptions resetOptions = new ResetOptions(true)
+        gameClient.resetGame(resetOptions)
+        gameClient.addGDIMinigunnerAtMapSquare(4,5)
+
+        Point mcvLocation = new Point(21,12)
+        gameClient.addMCVAtMapSquare(mcvLocation.x, mcvLocation.y)
+
+        when:
+        int testScenarioNumber = 16
+        int startX = 297
+        int startY = 92
+        int screenshotCompareWidth = 185
+        int screenshotCompareHeight = 229
+
+        def movements = [MovementDirection.NORTH, MovementDirection.NORTH, MovementDirection.NORTH,  MovementDirection.NORTH,
+                         MovementDirection.NORTHWEST, MovementDirection.NORTHWEST,
+                         MovementDirection.WEST, MovementDirection.WEST, MovementDirection.WEST, MovementDirection.WEST,
+                         MovementDirection.SOUTH, MovementDirection.SOUTH, MovementDirection.SOUTH, MovementDirection.SOUTH]
+
+        doMinigunnerPlacements(mcvLocation, movements)
+
+        then:
+        assertScreenshotMatches(testScenarioNumber, startX , startY, screenshotCompareWidth, screenshotCompareHeight)
+    }
+
 
     private void doMinigunnerPlacements(Point mcvLocation, List<MovementDirection> movements) {
         Point currentLocation = mcvLocation
